789a73b0d0fc9e2b754adbb39ed3ca92e9c30669
Binbin <binloveplay1314@qq.com>
2024-10-30 10:25:50 +0800

    Minor fix to debug logging in replicationFeedStreamFromPrimaryStream (#1235)
    We should only print logs when hide-user-data-from-log is off.
    
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- src/replication.c
+++ src/replication.c
@@ -651,7 +651,7 @@ void replicationFeedStreamFromPrimaryStream(char *buf, size_t buflen) {
     /* Debugging: this is handy to see the stream sent from primary
      * to replicas. Disabled with if(0). */
     if (0) {
-        if (server.hide_user_data_from_log) {
+        if (!server.hide_user_data_from_log) {
             printf("%zu:", buflen);
             for (size_t j = 0; j < buflen; j++) {
                 printf("%c", isprint(buf[j]) ? buf[j] : '.');
